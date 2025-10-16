642bcb30dedb56dde1705af00bb4eb170afc7829
nandomart <fernandomartinezc@p.marista.edu.mx>
2025-10-16 07:32:48 -0600

    cambio de prueba

--- test.cpp
+++ test.cpp
@@ -8,14 +8,6 @@ int main() {
     string nombre;
     int edad;
     
-    //cin >> nombre;
-    for(int i = 0; i < 3; i++){
-        cout << "Nombre: ";
-        getline(cin, nombre);
-        cout << "Edad: ";
-        cin >> edad;
-        cout << "El dato introducido es: " << nombre << ". Edad: " << edad << endl;
-        cin.ignore();
-    }
+    cout << "Hola soy Fernando.\n";
     
 }
